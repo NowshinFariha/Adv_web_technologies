6aa9d3480fad41b0eee61e164233f4b914161a10
Nowshin Fariha <158089893+NowshinFariha@users.noreply.github.com>
2025-06-03 22:32:38 +0600

    Add files via upload

---
+++ my_codes/Next/project/component/navbar.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>MyWebsite</div>
+      <ul className={styles.navLinks}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
